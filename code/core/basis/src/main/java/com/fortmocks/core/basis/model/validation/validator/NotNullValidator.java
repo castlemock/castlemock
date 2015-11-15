0be44abb576216cab315412c0ca430b7dd74478e
Karl Dahlgren <karl@dahlgren.info>
2015-11-15 13:44:10 +0100

    [FM-19] Updated the validators

--- code/core/basis/src/main/java/com/fortmocks/core/basis/model/validation/validator/NotNullValidator.java
+++ code/core/basis/src/main/java/com/fortmocks/core/basis/model/validation/validator/NotNullValidator.java
@@ -5,8 +5,6 @@ import com.fortmocks.core.basis.model.validation.NotNull;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.log4j.Logger;
 
-import java.lang.reflect.Field;
-
 /**
  * @author Karl Dahlgren
  * @since 1.0
@@ -15,21 +13,12 @@ public class NotNullValidator extends Validator {
 
     private static final Logger LOGGER = Logger.getLogger(NotNullValidator.class);
 
-    @Override
-    public <M extends Message> void validateMessage(final M message){
-        if(message == null){
-            throw new NullPointerException("The message cannot be null");
-        }
-
-        final Class<?> messageClass = message.getClass();
-        for(Field field : messageClass.getDeclaredFields()){
-            if(field.isAnnotationPresent(NotNull.class)){
-                validateNotNull(message, field.getName());
-            }
-        }
+    public NotNullValidator() {
+        super(NotNull.class);
     }
 
-    private  <M extends Message> void validateNotNull(final M message, final String field){
+    @Override
+    protected void validateMessage(final Message message, final String field){
         try {
             final Object object = FieldUtils.readField(message, field, true);
             if(object == null){
--- code/core/basis/src/main/java/com/fortmocks/core/basis/model/validation/validator/Validator.java
+++ code/core/basis/src/main/java/com/fortmocks/core/basis/model/validation/validator/Validator.java
@@ -2,12 +2,34 @@ package com.fortmocks.core.basis.model.validation.validator;
 
 import com.fortmocks.core.basis.model.Message;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
 /**
  * @author Karl Dahlgren
  * @since 1.0
  */
 public abstract class Validator {
 
-    public abstract <M extends Message> void validateMessage(final M message);
+    private Class<? extends Annotation> validationType;
+
+    protected Validator(Class<? extends Annotation> validationType){
+        this.validationType = validationType;
+    }
+
+    public void validateMessage(final Message message){
+        if(message == null){
+            throw new NullPointerException("The message cannot be null");
+        }
+
+        final Class<?> messageClass = message.getClass();
+        for(Field field : messageClass.getDeclaredFields()){
+            if(field.isAnnotationPresent(validationType)){
+                validateMessage(message, field.getName());
+            }
+        }
+    }
+
+    protected abstract <M extends Message> void validateMessage(final Message message, final String field);
 
 }
