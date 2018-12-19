d64707628b85c098ddc1c664ea931c1a9936f23d
Muhammad Hewedy <mhewedy@gmail.com>
2018-12-20 00:46:24 +0300

    [#90] adding author and license description

--- code/app/src/main/java/com/castlemock/app/config/MongoConfig.java
+++ code/app/src/main/java/com/castlemock/app/config/MongoConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Karl Dahlgren
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.castlemock.app.config;
 
 import com.castlemock.repository.Profiles;
@@ -11,6 +27,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
+/**
+ * Conditionally enable mongodb configurations.
+ *
+ * @author Mohammad Hewedy
+ * @since 1.35
+ */
 public class MongoConfig {
 
     @Configuration
