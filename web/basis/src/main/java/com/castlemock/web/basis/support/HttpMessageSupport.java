e75c77e3e1e62af16cedb218a9c501ee56de0dac
Meow <sander.schutten@gmail.com>
2020-02-14 07:21:40 +0000

    Also support NTLM 401 challenge and fix characterEncodoing

--- web/basis/src/main/java/com/castlemock/web/basis/support/HttpMessageSupport.java
+++ web/basis/src/main/java/com/castlemock/web/basis/support/HttpMessageSupport.java
@@ -264,7 +264,7 @@ public class HttpMessageSupport {
         BufferedReader bufferedReader = null;
         try {
 
-            if (connection.getResponseCode() >= OK_RESPONSE && connection.getResponseCode() < 500) {
+            if (connection.getResponseCode() >= OK_RESPONSE && connection.getResponseCode() < 400) {
                 // The connection returned a response with HTTP status 2xx.
                 // Use the input stream.
                 inputStream = connection.getInputStream();
@@ -281,7 +281,7 @@ public class HttpMessageSupport {
                 // The content is GZIP encoded.
                 // Create a decoder and parse the response.
                 final InputStream gzipStream = new GZIPInputStream(inputStream);
-                final Reader decoder = new InputStreamReader(gzipStream);
+                final Reader decoder = new InputStreamReader(gzipStream, characterEncoding);
                 bufferedReader = new BufferedReader(decoder);
             } else if(encodings.contains(ContentEncoding.DEFLATE)){
                 // The content is DEFLATE encoded.
@@ -291,7 +291,7 @@ public class HttpMessageSupport {
                 bufferedReader = new BufferedReader(decoder);
             } else {
                 // No encoding was used. Simply parse the response.
-                final InputStreamReader inputStreamReader = new InputStreamReader(inputStream, java.nio.charset.StandardCharsets.UTF_8);
+                final InputStreamReader inputStreamReader = new InputStreamReader(inputStream, characterEncoding);
                 bufferedReader = new BufferedReader(inputStreamReader);
             }
 
