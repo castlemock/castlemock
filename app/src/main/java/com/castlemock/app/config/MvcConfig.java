a96242cddb9ee7b3cdc0aedda6f5fa7ebe5c7b7c
Karl Dahlgren <karl@dahlgren.info>
2022-01-23 23:18:29 +0100

    Reverted Spring boot upgrade since it caused the redirect to stop function

--- app/src/main/java/com/castlemock/app/config/MvcConfig.java
+++ app/src/main/java/com/castlemock/app/config/MvcConfig.java
@@ -22,7 +22,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -33,7 +32,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @since 1.0
  */
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = { "com.castlemock" })
 public class MvcConfig implements WebMvcConfigurer {
 
